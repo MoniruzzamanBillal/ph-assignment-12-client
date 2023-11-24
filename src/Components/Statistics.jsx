a50a17e3b90ba5af662e74b62850431bd88b0965
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-11-24 12:34:46 +0600

    statistics section responsive done

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -11,22 +11,22 @@ const CounterData = [
   },
   {
     number: "90",
-    text: "Happy Users",
+    text: "Happy Customers",
   },
 ];
 
 const Statistics = () => {
   return (
-    <div className="CounterContainer bg-blue-400 py-8 ">
-      <div className="container">
-        <div className="counter w-[80%] m-auto ">
-          <div className="counterWrapper flex justify-evenly ">
+    <div className="CounterContainer bg-blue-200 py-8 ">
+      <div className="counterWrapper ">
+        <div className="counter  m-auto w-[99%] xsm:w-[95%] sm:w-[92%] ">
+          <div className="counterWrapper flex justify-between  m-auto ">
             {CounterData.map((ele, index) => (
               <div
-                className="counterItem flex flex-col justify-end items-end   w-[25%] text-gray-50 text-3xl font-semibold  "
+                className="counterItem flex flex-col items-center m-auto  w-[32%] text-gray-900 text-sm xsm:text-lg sm:text-xl md:text-2xl xmd:text-3xl font-semibold   "
                 key={index}
               >
-                <h3 className="counterNumber  ">{ele.number}+</h3>
+                <h3 className="counterNumber mb-1.5 ">{ele.number}+</h3>
                 <h4 className="counterTitle  ">{ele.text}</h4>
               </div>
             ))}
